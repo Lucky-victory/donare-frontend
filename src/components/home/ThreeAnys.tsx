3e932813a8bb3f9e7ff3cf90bde8926105231c8d
Lucky Victory <luckyvictory54@gmail.com>
2023-08-24 16:39:15 +0000

    changed the text at home page

--- src/components/home/ThreeAnys.tsx
+++ src/components/home/ThreeAnys.tsx
@@ -17,17 +17,14 @@ const ThreeAnys = () => {
       <div className="flex flex-col-reverse lg:flex-row justify-center items-center gap-4 w-full min-h-[400px] mt-8 lg:mt-16">
         <div className="w-full lg:w-1/2 h-full flex flex-col items-start justify-start lg:justify-center px-6">
           <h1 className="text-xl lg:text-4xl font-semibold pt-4 text-white">
-            Anyone, Anywhere, Anytime, Anychain!
+          Every Individual, Everywhere, Every Moment, Every Chain!
           </h1>
           <p className="text-white text-opacity-60 lg:text-lg pt-2">
-            Built for community causes, to help reputable individuals raise
-            funds for relevant issues as well as to fund grants. Donare is
-            not fragmented and limited to one chain, but accessible to users on
-            separate EVM chains.
+          Designed to support community-driven initiatives, Donare enables trustworthy individuals to gather resources for pressing concerns and fund grants. Donare's reach isn't confined to a single blockchain; it's accessible to users across various EVM chains.
           </p>
           <button className="long-btn-trans py-3 px-8 mt-4 lg:mt-8 flex items-center gap-2">
             <span className="uppercase">Learn More</span>
-            <ChevronRightIcon className="h-5 w-5 text-[#dba567]" />
+            <ChevronRightIcon className="h-5 w-5 text-[#d89e5b]" />
           </button>
         </div>
         <div className="w-full lg:w-1/2 h-full">
